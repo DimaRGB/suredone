294e10ee518740478bf35a505fe6aa888025bfe8
dima.rgb <dima.rgb@gmail.com>
2014-01-27 03:28:36 +0200

    ShipWorks module version 3.0.0

--- sw/shipworks.php
+++ sw/shipworks.php
@@ -1,5 +1,16 @@
 <?php
 
+	// init cycle
+	// Call 1: GetModule (action=getmodule) 
+	// Call 2: GetStore
+	// Call 3: GetStatusCodes
+
+	// download order cycle
+	// Call 1: GetModule
+	// Call 2: GetStatusCodes 
+	// Call 3: GetCount
+	// Call 4...N: GetOrders - виполняеться бесконечно пока не прийдет пустой ответ
+
 	function echoObj($obj) {
 		echo '<hr><xmp>';
 		print_r($obj);
@@ -8,36 +19,25 @@
 
 	class ShipWorksXML extends DOMDocument {
 
-		function __construct() {
+		private $moduleVersion = '3.0.0';
+		private $schemaVersion = '1.0.0';
+
+		public function __construct() {
+			parent::__construct('1.0', 'utf-8');
+			$this->xmlStandalone = true;
 			$this->formatOutput = true;
 			$this->preserveWhiteSpace = true;
+			$this->initRoot();
 		}
 
-		function load($filename, $options = 0) {
-			parent::load($filename, $options);
+		private function initRoot() {
+			$root = $this->createElement('ShipWorks');
+			$root->setAttribute('moduleVersion', $this->moduleVersion);
+			$root->setAttribute('schemaVersion', $this->schemaVersion);
+			$this->appendChild($root);
 		}
 
-		// function addBook($title, $author) {
-		// 	$id = $this->books->childNodes->length + 1;
-		// 	$book = $this->createElement('book');
-		// 	$book->appendChild($this->createElement('id', $id));
-		// 	$book->appendChild($this->createElement('title', $title));
-		// 	$book->appendChild($this->createElement('author', $author));
-		// 	$this->books->appendChild($book);
-		// }
-
-		// function removeBook() {
-		// 	if( $this->books->childNodes->length )
-		// 		$this->books->removeChild($this->books->firstChild);
-		// }
-
-		// function replaceBook($i1, $i2) {
-		// 	$book1 = $this->books->childNodes->item($i1);
-		// 	$book2 = $this->books->childNodes->item($i2);
-		// 	$this->books->replaceChild($book1, $book2);
-		// }
-
-		function saveAndEcho($isXML, $node = NULL) {
+		public function echoXML($isXML) {
 			if( $isXML ) {
 				// HTTP/1.1
 				header('Content-Type: text/xml; charset=utf-8');
@@ -45,22 +45,22 @@
 				header('Cache-Control: post-check=0, pre-check=0', false);
 				// HTTP/1.0
 				header('Pragma: no-cache');
-				echo $this->saveXML($node);
+				echo $this->saveXML();
 			} else
-				echo '<hr><xmp>' . $this->saveXML($node) . '</xmp>';
+				echo '<hr><xmp>' . $this->saveXML() . '</xmp>';
+			// $this->saveInHistory();
+		}
+
+		private function saveInHistory() {
+			$fileName = 'history/sw_'.
+				date('Y-m-d_H-i-s').
+				'-'.
+				(round(microtime(true) * 1000) % 1000).
+				'.xml';
+			$this->save($fileName);
 		}
 
 	}
 
 	$xml = new ShipWorksXML;
-	$xml->load('shipworks.xml');
-
-	// $action = $_REQUEST['action'];
-	// if( $action == 'add' )
-	// 	$xml->addBook('PHP Framework', 'Reza Christian');
-	// elseif( $action == 'remove' )
-	// 	$xml->removeBook();
-	// elseif( $action == 'replace' )
-	// 	$xml->replaceBook($_REQUEST['i1'], $_REQUEST['i2']);
-
-	$xml->saveAndEcho(true);
+	$xml->echoXML(true);
